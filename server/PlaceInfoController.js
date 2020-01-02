fc9e5a574a0829026ecc3d3ccd3d99985f2e0f67
nick theile <Nick@nicksmbp.local>
2020-01-01 19:19:51 -0600

    debugging and headcount

--- server/PlaceInfoController.js
+++ server/PlaceInfoController.js
@@ -1,33 +1,51 @@
 const express = require('express');
 const { decorateApp } = require('@awaitjs/express');
+import Geohash from 'latlon-geohash';
 
+const minPeopleCount = 5;
 
 const PlaceInfoController = (db) => {
   
   const Router = decorateApp(express.Router());
 
-  Router.getAsync('/headcount/:geohash', async (req, res) => {
-    const { geohash } = req.params;
+  Router.getAsync('/headcount/:lat/:long', async (req, res) => {
+    const { lat, long } = req.params;
 
-    var query = {_id: { $regex: geohash }};
+    // get all 8 hashes
+    let precision = 9;
+    const geohash9 = Geohash.encode(lat, long, precision);
+    const geohash8 = Geohash.encode(lat, long, 8);
+    const geohash = Geohash.encode(lat, long, 2);
+    
+    console.log('geohash9', geohash9);
+    var query = {_id: { $regex: geohash9 }};
+
     try{
-      db.collection("radiks-central-data").find(query).toArray(function(err, result) {
-        if (err) throw err;
-        console.log(result);
-        try{
-          const headcount = result[0].group.attrs.members.length;
-          res.json({ count: headcount });
-        } catch(e){
-          res.json({ error: e });
-        }
-      });
+      let headcount = await geohashQuery(query);
+      res.json({ count: headcount });
     } catch(e){
-      res.json({ error: e });
+     res.json({ error: e });
     }
+      
+
   });
 
+  async function geohashQuery(query){
+    let cursor = db.collection("radiks-central-data").find(query);
+    let result = await cursor.toArray();
+    console.log('result of query', result);
+    try{
+       const headcount = result[0].group.attrs.members.length;
+       return headcount;
+    } catch(e){
+      return 0;
+    }
+    
+  }
 
   return Router;
 };
+
+
 
 module.exports = PlaceInfoController;
